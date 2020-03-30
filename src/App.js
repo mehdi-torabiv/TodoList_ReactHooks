08c0d071628756c36f91377705081eb01bb5c19b
nimatorabi <46302001+nimatorabi@users.noreply.github.com>
2020-03-31 00:08:23 +0430

    create state[value of state , function for update state]=use...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Complete my homework",
+      isCompleted: false
+    },
+    {
+      text: "Doing exercise",
+      isCompleted: false
+    },
+    {
+      text: "Eat breakfast in tommorow morning!",
+      isCompleted: false
+    }
+  ]);
   return (
     <div>
 
